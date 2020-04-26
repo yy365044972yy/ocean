3f9c63da6bebdb3aa75126a6067f6ad3b6587ed1
liwentao <365044972@qq.com>
2020-04-26 11:11:35 +0800

    删除无用注解

--- src/main/java/com/hb/ocean/entity/UserRole.java
+++ src/main/java/com/hb/ocean/entity/UserRole.java
@@ -2,7 +2,6 @@ package com.hb.ocean.entity;
 
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
