c99f41eb5b35d5b0027db0ea715b30ef2a5c5413
liwentao <365044972@qq.com>
2020-05-07 17:10:04 +0800

    lombok

--- src/main/java/com/hb/ocean/entity/AbUser.java
+++ src/main/java/com/hb/ocean/entity/AbUser.java
@@ -2,17 +2,11 @@ package com.hb.ocean.entity;
 
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
 @Data
-@Getter
-@Setter
-@Component
 public class AbUser implements Serializable {
 
 
